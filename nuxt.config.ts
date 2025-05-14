28a0d35f466536cef58ef0c1abca8d0e2b738ce8
tzuling2001 <ling0967253511@gmail.com>
2025-05-14 17:26:57 +0800

    部署 gh-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,3 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
@@ -6,6 +5,7 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
   components: [{ path: "~/components", pathPrefix: false }],
   app: {
+    baseURL: "/Block-Studio-Test-Tz/",
     head: {
       link: [
         {
